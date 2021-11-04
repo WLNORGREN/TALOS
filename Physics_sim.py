4ace8790e0ce5df98b62e620b453dfaa5a0fc8fc
WLNORGREN <81774716+WLNORGREN@users.noreply.github.com>
2021-11-03 21:32:05 -0500

    Add files via upload
    Basic program demonstrating Latex and graphing

---
+++ Physics_sim.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  2 03:04:35 2021
+
+@author: WN
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 28 02:32:08 2021
+
+@author: WN
+"""
+
+import matplotlib
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import numpy as np
+
+
+matplotlib.use('TkAgg')
+
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
+    
+#Updating functions
+
+    
+#This function displays the function to be graphed in a Latex format to a matplotlib canvas
+def graph(event=None):
+    
+    tmptext = "F = {k}x^2"
+    tmptext = "$"+tmptext+"$"
+    
+    tmptext2 = "k = " + str(entry.get()) 
+    tmptext2 = "$"+tmptext2+"$"
+
+    ax.clear()
+    ax.text(0.2, 0.6, tmptext, fontsize=25)  
+    ax.text(0.2, 0.2, tmptext2, fontsize=25)
+    canvas.draw()
+    print("Test Phrase")
+    
+#This function displays the x-y graph to a matplotlib canvas    
+def plot(event=None):
+    
+    
+    x= np.arange(0, 3, .01)
+    bx.clear()
+    bx.plot(x, float(entry.get())*x**2)
+    canvas2.draw()
+    print("Hello World")
+    
+    
+
+#Tkinter setup
+root = tk.Tk()
+mainframe = tk.Frame(root)
+mainframe.pack()
+
+#Formula entry
+entry = tk.Entry(mainframe, width=70)
+entry.pack()
+entry.insert(0, "2")
+
+#Label entry
+label = tk.Label(mainframe)
+label.pack()
+#Quit button
+
+quitButton = tk.Button(root, text = "Quit", command = root.destroy)
+quitButton.pack()
+#Formula canvas
+fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
+ax = fig.add_subplot(111)
+
+
+canvas = FigureCanvasTkAgg(fig, master=label)
+canvas.get_tk_widget().pack(side="top", fill="both", expand=True)
+canvas._tkcanvas.pack(side="top", fill="both", expand=True)
+
+
+
+
+#Graph display
+fig2 = Figure(figsize=(5, 4), dpi=100)
+x= np.arange(0, 3, .01)
+bx = fig2.add_subplot(111)
+bx.plot(x, x**(int(entry.get())))
+
+canvas2 = FigureCanvasTkAgg(fig2, master=root)  # A tk.DrawingArea.
+canvas2.draw()
+canvas2.get_tk_widget().pack(side="top", fill="both", expand=True)
+
+
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+
+
+graph()
+plot()
+
+root.bind("<Return>", graph)
+root.bind("<Return>", plot, add = '+')
+root.mainloop()
