841c3fc8aaccf3a210dfc0a00716155b8f38bc23
wln <wln@pop-os.localdomain>
2025-02-05 19:55:42 -0500

    Updated postgres test

--- Astronomy_Projects/Exoplanet_Models/Planets_SQL_postgres_test.py
+++ Astronomy_Projects/Exoplanet_Models/Planets_SQL_postgres_test.py
@@ -12,7 +12,6 @@ import numpy as np
 from sqlalchemy import create_engine
 
 
-
 def infer_sql_type(series):
     """Infer SQLite data type based on a Pandas Series."""
     if np.issubdtype(series.dtype, np.integer):
@@ -21,6 +20,7 @@ def infer_sql_type(series):
         return "REAL"
     else:
         return "TEXT"
+
 
 # Step 1: Load CSV
 csv_file = '~/Documents/python_programs/Astronomy_Datasets/planets.csv'
@@ -31,6 +31,11 @@ df = pd.read_csv(csv_file)
 columns_with_types = [
     f"{col} {infer_sql_type(df[col])}" for col in df.columns
 ]
+
+columns_with_types_tuple = (
+    f"{col} {infer_sql_type(df[col])}" for col in df.columns
+)
+
 
 
 
@@ -43,30 +48,60 @@ try:
         host="localhost",  # or '127.0.0.1'
         port="5432"        # Default PostgreSQL port
     )
-    
-    engine = create_engine('postgresql://wln:2023$loup@localhost:5432/test_db')
+
+    engine = create_engine('postgresql://wln:2023$loup@localhost:5432/test')
 
     # Create a cursor object
     cursor = connection.cursor()
-    
+
     # Test the connection by fetching the version
     cursor.execute("SELECT version();")
     db_version = cursor.fetchone()
     print(f"Connected to: {db_version[0]}")
-    
+
     # Create new table for the planets database
-    
-    create_table_query = f"CREATE TABLE {table_name} ({', '.join(columns_with_types)});"
-    cursor.execute(f"DROP TABLE IF EXISTS {table_name};")  # Recreate table if it already exists
+
+    create_table_query = f"CREATE TABLE {
+        table_name} ({', '.join(columns_with_types)});"
+    # Recreate table if it already exists
+    cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
     cursor.execute(create_table_query)
-    
-    
 
     connection.commit()
     #  Insert the CSV data into the table
-    df.to_sql(table_name, engine,if_exists='append', index=False)
+    df.to_sql(table_name, engine, if_exists='append', index=False)
     #  Run an SQL query (e.g., select top 10 rows)
     query = f"SELECT * FROM {table_name} LIMIT 10;"
+    cursor.execute(query)
+
+    # Fetch and display results
+    results = cursor.fetchall()
+    columns = [desc[0]
+               for desc in cursor.description]  # Get column names for output
+
+    print(f"\nResults of the query:\n{columns}")
+    for row in results:
+        print(row)
+
+    query = f"SELECT * FROM {table_name} LIMIT 2;"
+    cursor.execute(query)
+
+    # Fetch and display results
+    results = cursor.fetchall()
+    columns = [desc[0]
+               for desc in cursor.description]  # Get column names for output
+
+    print(f"\nResults of the query:\n{columns}")
+    for row in results:
+        print(row)
+
+    # Update table
+    
+    a = 'e'
+    
+    query = f"UPDATE {table_name}  SET pl_letter = 'eee' WHERE rowid = 2;"
+    cursor.execute(query)
+    query = f"SELECT rowid, pl_letter FROM {table_name} ORDER BY rowid  LIMIT 4;"
     cursor.execute(query)
     
     # Fetch and display results
@@ -77,6 +112,49 @@ try:
     for row in results:
         print(row)
     
+    # Insert row
+    
+    query = f"INSERT INTO {table_name} (rowid) VALUES (44444);"
+    cursor.execute(query)
+    query = f"SELECT rowid, pl_letter FROM {table_name} ORDER BY rowid DESC LIMIT 2;"
+    cursor.execute(query)
+    
+    # Fetch and display results
+    results = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]  # Get column names for output
+
+    print(f"\nResults of the query:\n{columns}")
+    for row in results:
+        print(row)
+
+    # Alter table
+    query = f"ALTER TABLE {table_name} ADD galaxy CHAR"
+    cursor.execute(query)
+    query = f"SELECT rowid, pl_letter, galaxy FROM {table_name} ORDER BY rowid DESC LIMIT 2;"
+    cursor.execute(query)
+    
+    # Fetch and display results
+    results = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]  # Get column names for output
+
+    print(f"\nResults of the query:\n{columns}")
+    for row in results:
+        print(row)
+
+    # Delete from table
+    query = f"DELETE FROM {table_name} WHERE pl_letter = 'b' "
+    cursor.execute(query)
+    query = f"SELECT rowid, pl_letter, galaxy FROM {table_name} LIMIT 2;"
+    cursor.execute(query)
+    
+    # Fetch and display results
+    results = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]  # Get column names for output
+
+    print(f"\nResults of the query:\n{columns}")
+    for row in results:
+        print(row)
+
 
 except Exception as e:
     print(f"Error: {e}")
